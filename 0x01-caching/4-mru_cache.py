4297d8927ba68b4029a2af9738ded39c5f7002a6
Wongani Chulu <wonganichulu@gmail.com>
2024-10-30 18:21:38 +0200

    Added 4-mru_cache.py

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Module for defining a cache eviction policy
+"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """
+    MRU eviction policy class implementation that inherits
+    from BaseCaching
+    """
+
+    def __init__(self):
+        """
+        Initialises the class and its super class
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        Assigns key-value/item pair to a self.cache_data
+        """
+        if key is None or item is None:
+            return
+        if self.cache_data.__len__() >= self.MAX_ITEMS:
+            most_recent = list(self.cache_data)[-1]
+            print(f"DISCARD: {most_recent}")
+            del self.cache_data[most_recent]
+        self.cache_data[key] = item
+        return self.cache_data
+
+    def get(self, key):
+        """
+        Gets data using key if it exists
+        """
+        if key is None:
+            return
+        try:
+            item = self.cache_data[key]
+            del self.cache_data[key]
+            self.cache_data[key] = item
+            return item
+        except KeyError:
+            return None
