034a5fdaedd4b1d225e42e64334a198250a71c3c
Wongani Chulu <wonganichulu@gmail.com>
2024-11-05 12:50:44 +0200

    Added task 4 files

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Basic Hello world flask app
+"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+class Config:
+    """
+    Configuration class
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCAL = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Determine best match of a supported language"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """Route for the index page"""
+    return render_template('4-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
