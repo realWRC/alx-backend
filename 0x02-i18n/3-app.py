ff9ef78b4dd70b92bc6223fc9f2afe0538d8c09f
Wongani Chulu <wonganichulu@gmail.com>
2024-11-05 12:20:45 +0200

    Standby

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -29,7 +29,7 @@ def get_locale():
 @app.route('/')
 def index():
     """Route for the index page"""
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
