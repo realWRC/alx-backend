78db59f1226c9ecd8f0bbc8a50e1f70e987bb39f
Wongani Chulu <wonganichulu@gmail.com>
2024-12-04 03:54:21 +0200

    Added 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,33 @@
+// Connects to Redis
+
+import { createClient, print } from "redis";
+
+const client = createClient();
+
+client.on('error', () => {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+};
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (err, result) => {
+    if (err) {
+      console.log(error);
+      throw error;
+    }
+    console.log(result);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+client.quit();
